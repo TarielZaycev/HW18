f9925fe9383a17ebc0becda4bd66939260df55af
TarielZaycev <tarikzaycev@gmail.com>
2024-07-05 20:18:51 +0300

    rdy

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         //todo Реализовать класс Утка и Собака по описанным в ТЗ правилам.
+        Duck duck = new Duck(20.3,2,"уткаДаг");
+        Dog dog = new Dog(10.3,5,"Рекс");
+
+        dog.move();
+        duck.move();
+
+        dog.walk();
+        duck.walk();
     }
 }
